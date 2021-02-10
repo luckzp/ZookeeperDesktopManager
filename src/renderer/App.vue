35c23fdedbfbf26f9285e7725cbc0b8c3205140f
pengzhang34 <pengzhang34@iflytek.com>
2021-02-10 13:23:38 +0800

    更改项目名称

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -8,7 +8,7 @@
   import LandingPage from '@/pages/LandingPage'
 
   export default {
-    name: 'ZookeeperDesktopManger',
+    name: 'ZookeeperDesktopManager',
     components: {
       LandingPage
     }
